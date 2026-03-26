a090cef799135825a6330eb6d4e89a27567d8e59
zTenKeyLess <talan.kulkarni@gmail.com>
2026-03-27 08:58:31 +1300

    Update score.js

--- js/score.js
+++ js/score.js
@@ -24,7 +24,8 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
+    let baseScore = 300 - ((rank - 1) * (299 / 149));
+let score = baseScore *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
