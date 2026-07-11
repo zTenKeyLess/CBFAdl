8670276381cd1f20e65db8b96d994796f4f2e4de
zTenKeyLess <talan.kulkarni@gmail.com>
2026-07-11 17:38:32 +1200

    zoodac

--- js/score.js
+++ js/score.js
@@ -23,7 +23,7 @@ export function score(rank, percent, minPercent) {
     let score = (100 / Math.sqrt((rank - 1) / 50 + 0.444444) - 50) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
-  let total = 90;
+  let total = 91;
 let exponent = 2;
 
 let t = (rank - 1) / (total - 1);
@@ -35,9 +35,9 @@ if (rank === 1) {
   baseScore *= 1.20;
 } else if (rank === 4) {
   baseScore *= 1.01; // keeps #4 ahead
-} else if (rank === 15) {
+} else if (rank === 16) {
   baseScore *= 1.01;
-} else if (rank === 20) {
+} else if (rank === 21) {
   baseScore *= 1.02;
 }
 
