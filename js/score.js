1a297da099c5f5fb465294f52c75f833d5994bfb
zTenKeyLess <talan.kulkarni@gmail.com>
2026-09-05 19:48:27 +0800

    gabgai

--- js/score.js
+++ js/score.js
@@ -23,7 +23,7 @@ export function score(rank, percent, minPercent) {
     let score = (100 / Math.sqrt((rank - 1) / 50 + 0.444444) - 50) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
-  let total = 97;
+  let total = 98;
 let exponent = 2;
 
 let t = (rank - 1) / (total - 1);
